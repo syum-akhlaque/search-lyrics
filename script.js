5f1df3cea837117610b182c9f6996932f5c9f368
syum-akhlaque <arssyum@gmail.com>
2020-08-17 15:17:20 +0600

    add  fency relult

--- script.js
+++ script.js
@@ -8,15 +8,27 @@ function getApi()
       
 }
   function displayUser(data){
-      
+    document.getElementById("lyricsList").innerText = "";
     for (let i = 0; i < 10; i++) {
         let abbumTitle = data.data[i].title;
         let artistName = data.data[i].artist.name;
         let lyricsApi = `https://api.lyrics.ovh/v1/${artistName}/${abbumTitle}`;
-        
         const div = document.createElement("div");
         const lyricsList = document.getElementById("lyricsList");
-        div.innerHTML = `<p class="author lead"><strong id="albumTitle">  ${abbumTitle} </strong> Album by<span id="artistName">  ${artistName}  </span> <button class="btn btn-success" onclick = 'getLyrics("${lyricsApi}")'> Get Lyrics</button> </p>`; 
+        //div.innerHTML = `<p class="author lead"><strong id="albumTitle">  ${abbumTitle} </strong> Album by<span id="artistName">  ${artistName}  </span> <button class="btn btn-success" onclick = 'getLyrics("${lyricsApi}")'> Get Lyrics</button> </p>`; 
+
+        
+        div.innerHTML = `<div class="single-result row align-items-center my-3 p-3">
+                          <div class="col-md-9">
+                              <h3 class="lyrics-name">${abbumTitle}</h3>
+                              <p class="author lead">Album by <span>${artistName}</span></p>
+                          </div>
+                          <div class="col-md-3 text-md-right text-center">
+                              <button class="btn btn-success" onclick = 'getLyrics("${lyricsApi}")' >Get Lyrics</button>
+                          </div>
+                      </div>`
+
+
         lyricsList.appendChild(div);      
     }
 
@@ -28,7 +40,6 @@ function getApi()
       {
         let getLyrics = document.getElementById("getLyrics");
         getLyrics.innerHTML = data.lyrics;
-        console.log(data.lyrics);  
       }
       )
   }
